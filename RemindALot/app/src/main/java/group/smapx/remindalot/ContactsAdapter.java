55f569ff8c2de533ec2ef2b3340b3a162fd769cb
dentych <dennis@tychsen.me>
2016-10-06 16:58:10 +0200

    Moar better ShowActivity implementation

---
+++ RemindALot/app/src/main/java/group/smapx/remindalot/ContactsAdapter.java
@@ -0,0 +1,36 @@
+package group.smapx.remindalot;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class ContactsAdapter extends ArrayAdapter<Contact> {
+    public ContactsAdapter(Context context) {
+        super(context, android.R.layout.simple_list_item_1);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        if (convertView == null) {
+            LayoutInflater li = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = li.inflate(R.layout.view_contact, null);
+        }
+
+        TextView name = (TextView) v.findViewById(R.id.name);
+        TextView phoneNumber = (TextView) v.findViewById(R.id.phoneNumber);
+        Contact contact = getItem(position);
+
+        if (contact != null) {
+            name.setText(contact.getName());
+            phoneNumber.setText(contact.getPhoneNumber());
+        }
+
+        return v;
+    }
+}
--- RemindALot/app/src/main/java/group/smapx/remindalot/MainActivity.java
+++ RemindALot/app/src/main/java/group/smapx/remindalot/MainActivity.java
@@ -6,6 +6,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final ListView listReminder = (ListView) findViewById(R.id.listReminders);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab_create = (FloatingActionButton) findViewById(R.id.fab);
@@ -27,6 +35,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ArrayAdapter<Reminder> adapter = new ArrayAdapter<Reminder>(this, android.R.layout.simple_list_item_1);
+        ArrayList<Contact> contacts = new ArrayList<>();
+        contacts.add(new Contact("Annedreas", "25010203"));
+        contacts.add(new Contact("Abekat", "12312302"));
 
+        Reminder reminder = new Reminder("Titel", "Forklaring", new Date().getTime(), contacts);
+        adapter.add(reminder);
+        listReminder.setAdapter(adapter);
+
+        listReminder.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Reminder item = (Reminder) listReminder.getItemAtPosition(position);
+
+                Intent intent = new Intent(view.getContext(), ShowActivity.class);
+                intent.putExtra("reminder", item);
+                startActivity(intent);
+            }
+        });
     }
 }
